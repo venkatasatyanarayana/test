d877fee4e0a5c9d5ee7deba6261d4b4fbf085d42
VENKATA <chvs2020@gmail.com>
2021-01-26 18:06:07 +0530

    Test Application

---
+++ Demo/src/main/IntersectingArray.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntersectingArray {
+	public List<Integer> findInterctingElements(int[] array1, int[] array2){
+		List<Integer> result = new ArrayList<>();
+		int[] source;
+		int[] destination;
+		if(array1.length>array2.length){
+			source = array1;
+			destination = array2;
+		}else{
+			source = array2;
+			destination = array1;
+		}
+				for(int index=0;index<source.length;index++){
+					for(int dindex=0;dindex<destination.length;dindex++){
+						if(source[index]==destination[dindex]){
+							result.add(source[index]);
+						}
+					}
+			
+			}
+				return result;
+	}
+}
---
+++ Demo/src/test/TestIntersectingArray.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.util.List;
+
+import main.IntersectingArray;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class TestIntersectingArray {
+	
+	@Test
+	public void posTestFindInterctingElements(){
+		int array1[] = {1,3,5,9,10};
+		int array2[]={3,9,2,4,8,9,6};
+		IntersectingArray intersectArray = new IntersectingArray();
+		List<Integer> result = intersectArray.findInterctingElements(array1, array2);
+		System.out.println("posTestFindInterctingElements result="+result);
+		Assert.assertEquals(true, result.contains(3));
+		Assert.assertEquals(true, result.contains(9));
+	}
+	
+	@Test
+	public void negTestFindInterctingElements(){
+		int array1[] = {1,3,5,9,10};
+		int array2[]={2,4,6,8};
+		IntersectingArray intersectArray = new IntersectingArray();
+		List<Integer> result = intersectArray.findInterctingElements(array1, array2);
+		System.out.println("negTestFindInterctingElements result="+result);
+		Assert.assertEquals(true, result.isEmpty());
+	}
+
+}
