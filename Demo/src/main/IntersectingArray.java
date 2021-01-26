ef2e078dc4867cdec436efc35af3c0f12de1e8ad
VENKATA <chvs2020@gmail.com>
2021-01-26 18:23:29 +0530

    Updated array size logic

--- Demo/src/main/IntersectingArray.java
+++ Demo/src/main/IntersectingArray.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class IntersectingArray {
 	public List<Integer> findInterctingElements(int[] array1, int[] array2){
-		List<Integer> response = new ArrayList<>();
+		List<Integer> result = new ArrayList<>();
 		int[] source;
 		int[] destination;
 		if(array1.length>array2.length){
@@ -15,14 +15,16 @@ public class IntersectingArray {
 			source = array2;
 			destination = array1;
 		}
-				for(int index=0;index<source.length;index++){
-					for(int dindex=0;dindex<destination.length;dindex++){
+		int sourceSize = source.length;
+		int destSize = destination.length;
+				for(int index=0;index<sourceSize;index++){
+					for(int dindex=0;dindex<destSize;dindex++){
 						if(source[index]==destination[dindex]){
-							response.add(source[index]);
+							result.add(source[index]);
 						}
 					}
 			
 			}
-				return response;
+				return result;
 	}
 }
